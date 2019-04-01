7e458c741c6be6b9e68c7163250f19ad2e35abf5
Laurence <gabrielrebadulla@gmail.com>
2019-04-01 21:41:12 +0800

    to be deployed

--- blog/models.py
+++ blog/models.py
@@ -14,9 +14,19 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    publication_date = models.DateTimeField('date published')
-    body = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]+'...'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
 
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Blog
+
+def all_blogs(request):
+    blog = Blog.objects
+    return render(request, 'blog/all_blogs.html', {'blog':blog})
+
+def detail(request, blog_id):
+    detail_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':detail_blog})
